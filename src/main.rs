f8ca64f887a882d4916ce5142ca8356e79cb816c
Julian Bieber <julian.bieber.1992@gmail.com>
2020-10-10 22:54:56 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::shape,
+        pipeline::{DynamicBinding, PipelineDescriptor, PipelineSpecialization, RenderPipeline},
+        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
+        renderer::RenderResources,
+        shader::{ShaderStage, ShaderStages},
+    },
+};
+mod material;
+mod shaders;
+use material::{
+    CustomMaterial,
+    update_material_time
+};
+use shaders::*;
+
+fn main() {
+    App::build()
+        .add_default_plugins()
+        .add_asset::<CustomMaterial>()
+        .add_startup_system(setup.system())
+        .add_system(update_material_time.system())
+        .run();
+}
+fn setup(
+    mut commands: Commands,
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut shaders: ResMut<Assets<Shader>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
+) {
+    // Create a new shader pipeline
+    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, VERTEX_SHADER)),
+        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGMENT_SHADER))),
+    }));
+
+    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to our shader
+    render_graph.add_system_node(
+        "my_material",
+        AssetRenderResourcesNode::<CustomMaterial>::new(true),
+    );
+
+    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This ensures "my_material" runs before the main pass
+    render_graph
+        .add_node_edge("my_material", base::node::MAIN_PASS)
+        .unwrap();
+
+    // Create a new material
+    let material = materials.add(CustomMaterial {
+        time: 0.0f32,
+    });
+
+    // Setup our world
+    commands
+        // cube
+        .spawn(MeshComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::specialized(
+                pipeline_handle,
+                // NOTE: in the future you wont need to manually declare dynamic bindings
+                PipelineSpecialization {
+                    dynamic_bindings: vec![
+                        // Transform
+                        DynamicBinding {
+                            bind_group: 1,
+                            binding: 0,
+                        },
+                        // MyMaterial_color
+                        DynamicBinding {
+                            bind_group: 1,
+                            binding: 1,
+                        },
+                    ],
+                    ..Default::default()
+                },
+            )]),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with(material)
+        // camera
+        .spawn(Camera3dComponents {
+            transform: Transform::new(Mat4::face_toward(
+                Vec3::new(3.0, 5.0, -8.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            )),
+            ..Default::default()
+        });
+}
