10a23d6734cebe8ab83a12cee515fb17b6bca80a
Julian Bieber <julian.bieber.1992@gmail.com>
2020-10-12 22:06:34 +0200

    move water

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-mod material;
-mod shaders;
+mod water;
 mod world;
 
-use bevy::{
-    prelude::*,
-    render::{
-        mesh::shape,
-        pipeline::{DynamicBinding, PipelineDescriptor, PipelineSpecialization, RenderPipeline},
-        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
-        shader::{ShaderStage, ShaderStages},
-    },
-};
+use bevy::prelude::*;
 use physme::prelude3d::*;
 
-use material::{update_material_time, CustomMaterial};
-use shaders::*;
+use water::{setup_water_layer, update_material_time, WaterMaterial};
 use world::world_setup;
 
 fn main() {
@@ -24,68 +14,16 @@ fn main() {
         .add_resource(GlobalGravity(Vec3::new(0.0, -9.8, 0.0)))
         .add_resource(GlobalFriction(0.90))
         .add_resource(GlobalStep(0.5))
-        .add_asset::<CustomMaterial>()
+        .add_asset::<WaterMaterial>()
         .add_startup_system(setup.system())
         .add_startup_system(world_setup.system())
+        .add_startup_system(setup_water_layer.system())
         .add_system(update_material_time.system())
         .run();
 }
-fn setup(
-    mut commands: Commands,
-    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
-    mut shaders: ResMut<Assets<Shader>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
-    mut render_graph: ResMut<RenderGraph>,
-) {
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     // Create a new shader pipeline
-    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
-        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, VERTEX_SHADER)),
-        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGMENT_SHADER))),
-    }));
-
-    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to our shader
-    render_graph.add_system_node(
-        "my_material",
-        AssetRenderResourcesNode::<CustomMaterial>::new(true),
-    );
-
-    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This ensures "my_material" runs before the main pass
-    render_graph
-        .add_node_edge("my_material", base::node::MAIN_PASS)
-        .unwrap();
-
-    // Create a new material
-    let material = materials.add(CustomMaterial { time: 0.0f32 });
-
-    // Setup our world
     commands
-        // cube
-        .spawn(MeshComponents {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::specialized(
-                pipeline_handle,
-                // NOTE: in the future you wont need to manually declare dynamic bindings
-                PipelineSpecialization {
-                    dynamic_bindings: vec![
-                        // Transform
-                        DynamicBinding {
-                            bind_group: 1,
-                            binding: 0,
-                        },
-                        // MyMaterial_color
-                        DynamicBinding {
-                            bind_group: 1,
-                            binding: 1,
-                        },
-                    ],
-                    ..Default::default()
-                },
-            )]),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-            ..Default::default()
-        })
-        .with(material)
         // camera
         .spawn(Camera3dComponents {
             transform: Transform::new(Mat4::face_toward(
---
+++ src/water.rs
@@ -0,0 +1,113 @@
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::shape,
+        pipeline::{DynamicBinding, PipelineDescriptor, PipelineSpecialization, RenderPipeline},
+        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
+        renderer::RenderResources,
+        shader::{ShaderStage, ShaderStages},
+    },
+};
+#[derive(RenderResources)]
+pub struct WaterMaterial {
+    pub time: f32,
+}
+
+impl WaterMaterial {
+    pub fn add(&mut self) {
+        self.time += 1.0f32;
+    }
+}
+
+pub fn update_material_time(
+    mut material: ResMut<Assets<WaterMaterial>>,
+    handle: Mut<Handle<WaterMaterial>>,
+) {
+    for m in material.get_mut(&handle).iter_mut() {
+        m.add();
+    }
+}
+
+pub fn setup_water_layer(
+    mut commands: Commands,
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut shaders: ResMut<Assets<Shader>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<WaterMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
+) {
+    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, VERTEX_SHADER)),
+        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGMENT_SHADER))),
+    }));
+
+    render_graph.add_system_node(
+        "water_material",
+        AssetRenderResourcesNode::<WaterMaterial>::new(true),
+    );
+
+    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This ensures "my_material" runs before the main pass
+    render_graph
+        .add_node_edge("water_material", base::node::MAIN_PASS)
+        .unwrap();
+
+    // Create a new material
+    let material = materials.add(WaterMaterial { time: 0.0f32 });
+
+    // Setup our world
+    commands
+        // cube
+        .spawn(MeshComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::specialized(
+                pipeline_handle,
+                // NOTE: in the future you wont need to manually declare dynamic bindings
+                PipelineSpecialization {
+                    dynamic_bindings: vec![
+                        // Transform
+                        DynamicBinding {
+                            bind_group: 1,
+                            binding: 0,
+                        },
+                        // MyMaterial_color
+                        DynamicBinding {
+                            bind_group: 1,
+                            binding: 1,
+                        },
+                    ],
+                    ..Default::default()
+                },
+            )]),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with(material);
+}
+
+const VERTEX_SHADER: &str = r#"
+#version 450
+layout(location = 0) in vec3 Vertex_Position;
+layout(set = 0, binding = 0) uniform Camera {
+    mat4 ViewProj;
+};
+layout(set = 1, binding = 0) uniform Transform {
+    mat4 Model;
+};
+layout(set = 1, binding = 1) uniform WaterMaterial_time {
+    float time;
+};
+void main() {
+    gl_Position = ViewProj * Model * vec4(Vertex_Position.x, Vertex_Position.y + sin(time * 0.1 + Vertex_Position.x), Vertex_Position.z, 1.0);
+}
+"#;
+
+const FRAGMENT_SHADER: &str = r#"
+#version 450
+layout(location = 0) out vec4 o_Target;
+layout(set = 1, binding = 1) uniform WaterMaterial_time {
+    float time;
+};
+void main() {
+    o_Target = vec4(0, 0, (sin(gl_FragCoord.x + gl_FragCoord.y + time) + 1) / 2 * 255, 1.0);
+}
+"#;
