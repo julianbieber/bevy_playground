670fb1c97e36f63a7ed5c3b8318ab57f9c97c41d
Yasemin Göksu <yasemingoksu@eduroam120.igd.fraunhofer.de>
2020-10-25 17:13:17 +0100

    Player is a cube

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, PrintDiagnosticsPlugin};
 use bevy::prelude::*;
 use physme::prelude3d::*;
 
-use bevy::input::mouse::{MouseMotion, MouseWheel};
-use bevy::render::camera::Camera;
+use bevy::input::mouse::MouseMotion;
 use water::body_of_water::{
     set_water_position, setup_water_layer, update_material_time, WaterMaterial,
 };
@@ -14,20 +13,90 @@ use water::water_effect::apply_water_raise;
 use world::world_setup;
 
 #[derive(Default)]
-struct InputState {
-    pub reader_motion: EventReader<MouseMotion>,
-    pub reader_scroll: EventReader<MouseWheel>,
-}
-#[derive(Default)]
 struct State {
     mouse_motion_event_reader: EventReader<MouseMotion>,
-    mouse_wheel_event_reader: EventReader<MouseWheel>,
 }
 
 #[derive(Default)]
 struct CameraRotation {
     rotation_x: f32,
     rotation_y: f32,
+}
+
+/// this component indicates what entities should rotate
+#[derive(Default)]
+struct Rotator;
+
+/// rotates the parent, which will result in the child also rotating
+fn rotator_system(
+    mut state: ResMut<State>,
+    mut camera_rotation: ResMut<CameraRotation>,
+    mouse_motion_events: Res<Events<MouseMotion>>,
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<(&Rotator, &mut Transform)>,
+) {
+    for (_rotator, mut transform) in &mut query.iter() {
+        for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
+            let look = event.delta;
+            let camera_translation = transform.translation;
+            transform.translation = Vec3::zero();
+            if (f32::rem_euclid(
+                camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
+                2.0 * std::f32::consts::PI,
+            ) - std::f32::consts::FRAC_PI_2)
+                .abs()
+                < 0.2
+            {
+            } else if (f32::rem_euclid(
+                camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
+                2.0 * std::f32::consts::PI,
+            ) - 3.0 * std::f32::consts::FRAC_PI_2)
+                .abs()
+                < 0.2
+            {
+            } else {
+                camera_rotation.rotation_y = f32::rem_euclid(
+                    camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
+                    2.0 * std::f32::consts::PI,
+                );
+            };
+            camera_rotation.rotation_x = f32::rem_euclid(
+                camera_rotation.rotation_x - (look.x()).to_radians() / 5.0,
+                2.0 * std::f32::consts::PI,
+            );
+            transform.rotation = Quat::from_rotation_ypr(
+                camera_rotation.rotation_x,
+                camera_rotation.rotation_y,
+                0.0,
+            );
+            transform.translation = camera_translation;
+        }
+
+        if keys.pressed(KeyCode::W) {
+            let a = transform.rotation.mul_vec3(Vec3::new(0.0, 0.0, -1.0));
+            transform.translation += a;
+        }
+        if keys.pressed(KeyCode::A) {
+            let a = transform.rotation.mul_vec3(Vec3::new(-1.0, 0.0, 0.0));
+            transform.translation += a;
+        }
+        if keys.pressed(KeyCode::D) {
+            let a = transform.rotation.mul_vec3(Vec3::new(1.0, 0.0, 0.0));
+            transform.translation += a;
+        }
+        if keys.pressed(KeyCode::S) {
+            let a = transform.rotation.mul_vec3(Vec3::new(0.0, 0.0, 1.0));
+            transform.translation += a;
+        }
+        if keys.pressed(KeyCode::Q) {
+            let a = transform.rotation.mul_vec3(Vec3::new(0.0, 1.0, 0.0));
+            transform.translation += a;
+        }
+        if keys.pressed(KeyCode::E) {
+            let a = transform.rotation.mul_vec3(Vec3::new(0.0, -1.0, 0.0));
+            transform.translation += a;
+        }
+    }
 }
 
 fn main() {
@@ -46,16 +115,19 @@ fn main() {
         .add_system(update_material_time.system())
         .add_system(set_water_position.system())
         .add_system(apply_water_raise.system())
-        .add_system(camera_translation.system())
+        .add_system(rotator_system.system())
         .init_resource::<State>()
         .init_resource::<CameraRotation>()
-        .init_resource::<InputState>()
-        .add_system(process_mouse_events.system())
+        .init_resource::<Rotator>()
         .add_system(bevy::input::system::exit_on_esc_system.system())
-        .add_system(camera_translation.system())
         .run();
 }
-fn setup(mut commands: Commands, mut windows: ResMut<Windows>) {
+fn setup(
+    mut commands: Commands,
+    mut windows: ResMut<Windows>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     windows
         .get_primary_mut()
         .unwrap()
@@ -64,90 +136,27 @@ fn setup(mut commands: Commands, mut windows: ResMut<Windows>) {
         .get_primary_mut()
         .unwrap()
         .set_cursor_visibility(false);
-    commands
-        // camera
-        .spawn(Camera3dComponents {
-            transform: Transform::from_matrix(Mat4::face_toward(
-                Vec3::new(3.0, 5.0, -8.0),
-                Vec3::new(0.0, 0.0, 0.0),
-                Vec3::new(0.0, 1.0, 0.0),
-            )),
-            ..Default::default()
-        });
-}
-fn process_mouse_events(
-    mut state: ResMut<State>,
-    mut camera_rotation: ResMut<CameraRotation>,
-    mouse_motion_events: Res<Events<MouseMotion>>,
-    mouse_wheel_events: Res<Events<MouseWheel>>,
-    _camera: &Camera,
-    mut transform: Mut<Transform>,
-) {
-    for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
-        let look = event.delta;
-        let camera_translation = transform.translation;
-        transform.translation = Vec3::zero();
-        if (f32::rem_euclid(
-            camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
-            2.0 * std::f32::consts::PI,
-        ) - std::f32::consts::FRAC_PI_2)
-            .abs()
-            < 0.2
-        {
-        } else if (f32::rem_euclid(
-            camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
-            2.0 * std::f32::consts::PI,
-        ) - 3.0 * std::f32::consts::FRAC_PI_2)
-            .abs()
-            < 0.2
-        {
-        } else {
-            camera_rotation.rotation_y = f32::rem_euclid(
-                camera_rotation.rotation_y - (look.y()).to_radians() / 5.0,
-                2.0 * std::f32::consts::PI,
-            );
-        };
-        camera_rotation.rotation_x = f32::rem_euclid(
-            camera_rotation.rotation_x - (look.x()).to_radians() / 5.0,
-            2.0 * std::f32::consts::PI,
-        );
-        transform.rotation =
-            Quat::from_rotation_ypr(camera_rotation.rotation_x, camera_rotation.rotation_y, 0.0);
-        transform.translation = camera_translation;
-    }
 
-    for event in state.mouse_wheel_event_reader.iter(&mouse_wheel_events) {
-        let _zoom_delta = event.y;
-    }
-}
-fn camera_translation(
-    _camera_rotation: Res<CameraRotation>,
-    keys: Res<Input<KeyCode>>,
-    _camera: &Camera,
-    mut transform: Mut<Transform>,
-) {
-    if keys.pressed(KeyCode::W) {
-        let a = transform.rotation.mul_vec3(Vec3::new(0.0, 0.0, -1.0));
-        transform.translation += a;
-    }
-    if keys.pressed(KeyCode::A) {
-        let a = transform.rotation.mul_vec3(Vec3::new(-1.0, 0.0, 0.0));
-        transform.translation += a;
-    }
-    if keys.pressed(KeyCode::D) {
-        let a = transform.rotation.mul_vec3(Vec3::new(1.0, 0.0, 0.0));
-        transform.translation += a;
-    }
-    if keys.pressed(KeyCode::S) {
-        let a = transform.rotation.mul_vec3(Vec3::new(0.0, 0.0, 1.0));
-        transform.translation += a;
-    }
-    if keys.pressed(KeyCode::Q) {
-        let a = transform.rotation.mul_vec3(Vec3::new(0.0, 1.0, 0.0));
-        transform.translation += a;
-    }
-    if keys.pressed(KeyCode::E) {
-        let a = transform.rotation.mul_vec3(Vec3::new(0.0, -1.0, 0.0));
-        transform.translation += a;
-    }
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 0.2 }));
+    let cube_material_handle = materials.add(StandardMaterial {
+        albedo: Color::rgb(0.8, 0.7, 0.6),
+        ..Default::default()
+    });
+
+    commands
+        // parent cube
+        .spawn(PbrComponents {
+            mesh: cube_handle.clone(),
+            material: cube_material_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with(Rotator)
+        .with_children(|parent| {
+            parent.spawn(Camera3dComponents {
+                transform: Transform::from_translation(Vec3::new(0.0, 4.0, 4.0))
+                    .looking_at(Vec3::default(), Vec3::unit_y()),
+                ..Default::default()
+            });
+        });
 }
