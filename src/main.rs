c269024661e92be5689811708adecd7fcab82473
Julian Bieber <j.bieber@patagona.de>
2021-01-04 23:12:44 +0100

    render many small cubes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 mod ai;
 mod input;
 mod movement;
+mod particles;
 mod physics;
 mod vec3_ext;
 mod voxel_world;
 mod water;
 mod world;
 
-use bevy::diagnostic::DiagnosticsPlugin;
 use bevy::prelude::*;
 
 use crate::ai::movement::{npc_movement_system, update_behaviour_system};
 use crate::ai::spawn::{enemy_spawn_system, SpawnCoolDown};
 use crate::input::{publish_player_movements, MouseEvents};
 use crate::movement::{MoveEvent, MovementReader};
+use crate::particles::ParticlePlugin;
 use crate::physics::collider::collision_update;
 use movement::{movement_system, player_setup};
 use voxel_world::collision::systems::terrain_collision_system;
@@ -26,7 +27,7 @@ use world::world_setup;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_plugin(DiagnosticsPlugin::default())
+        .add_plugin(ParticlePlugin)
         .add_resource(SpawnCoolDown {
             timer: Timer::from_seconds(2.0, true),
         })
