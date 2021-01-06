aeaacd7198974fcfb4b89353172f02b447b036b6
Julian Bieber <j.bieber@patagona.de>
2021-01-06 22:04:23 +0100

    water plugin

--- src/main.rs
+++ src/main.rs
@@ -16,32 +16,25 @@ use crate::input::{publish_player_movements, MouseEvents};
 use crate::movement::{MoveEvent, MovementReader};
 use crate::particles::ParticlePlugin;
 use crate::physics::collider::collision_update;
+use crate::water::WaterPlugin;
 use movement::{movement_system, player_setup};
 use voxel_world::collision::systems::terrain_collision_system;
-use water::body_of_water::{
-    set_water_position, setup_water_layer, update_material_time, WaterMaterial,
-};
-use water::water_effect::apply_water_raise;
 use world::world_setup;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(ParticlePlugin)
+        .add_plugin(WaterPlugin)
         .add_resource(SpawnCoolDown {
             timer: Timer::from_seconds(2.0, true),
         })
-        .add_asset::<WaterMaterial>()
         .add_startup_system(setup.system())
         .add_startup_system(world_setup.system())
         .add_startup_system(player_setup.system())
-        .add_startup_system(setup_water_layer.system())
         .add_system(publish_player_movements.system())
         .add_system(npc_movement_system.system())
         .add_system(update_behaviour_system.system())
-        .add_system(update_material_time.system())
-        .add_system(set_water_position.system())
-        .add_system(apply_water_raise.system())
         .add_system(movement_system.system())
         .add_system(enemy_spawn_system.system())
         .init_resource::<MouseEvents>()
