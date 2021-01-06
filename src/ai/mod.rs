a30f50b42cad098bff297b2c9fae509ad2008e4a
Julian Bieber <j.bieber@patagona.de>
2021-01-06 22:09:03 +0100

    ai plugin

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,3 +1,20 @@
-pub mod model;
-pub mod movement;
-pub mod spawn;
+mod model;
+mod movement;
+mod spawn;
+
+use crate::ai::movement::{npc_movement_system, update_behaviour_system};
+use crate::ai::spawn::{enemy_spawn_system, SpawnCoolDown};
+use bevy::prelude::*;
+
+pub struct AIPlugin;
+
+impl Plugin for AIPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(SpawnCoolDown {
+            timer: Timer::from_seconds(2.0, true),
+        })
+        .add_system(npc_movement_system.system())
+        .add_system(update_behaviour_system.system())
+        .add_system(enemy_spawn_system.system());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ mod world;
 
 use bevy::prelude::*;
 
-use crate::ai::movement::{npc_movement_system, update_behaviour_system};
-use crate::ai::spawn::{enemy_spawn_system, SpawnCoolDown};
+use crate::ai::AIPlugin;
 use crate::input::{publish_player_movements, MouseEvents};
 use crate::movement::{MoveEvent, MovementReader};
 use crate::particles::ParticlePlugin;
@@ -26,17 +25,12 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(ParticlePlugin)
         .add_plugin(WaterPlugin)
-        .add_resource(SpawnCoolDown {
-            timer: Timer::from_seconds(2.0, true),
-        })
-        .add_startup_system(setup.system())
+        .add_plugin(AIPlugin)
+        .add_startup_system(window_setup.system())
         .add_startup_system(world_setup.system())
         .add_startup_system(player_setup.system())
         .add_system(publish_player_movements.system())
-        .add_system(npc_movement_system.system())
-        .add_system(update_behaviour_system.system())
         .add_system(movement_system.system())
-        .add_system(enemy_spawn_system.system())
         .init_resource::<MouseEvents>()
         .add_event::<MoveEvent>()
         .init_resource::<MovementReader>()
@@ -46,7 +40,7 @@ fn main() {
         .run();
 }
 
-fn setup(mut windows: ResMut<Windows>) {
+fn window_setup(mut windows: ResMut<Windows>) {
     windows
         .get_primary_mut()
         .unwrap()
