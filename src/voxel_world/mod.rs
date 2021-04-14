243ae31474226a98c49f942ddc66b208c666babb
Julian Bieber <julian.bieber.1992@gmail.com>
2021-04-14 22:55:54 +0200

    update bevy; earlier lod

--- src/voxel_world/mod.rs
+++ src/voxel_world/mod.rs
@@ -11,15 +11,17 @@ pub mod voxel;
 pub fn distance_2_lod(distance: f32) -> i32 {
     if distance < 300.0 {
         1
-    } else if distance < 600.0 {
+    } else if distance < 500.0 {
         2
-    } else if distance < 1200.0 {
+    } else if distance < 700.0 {
         4
-    } else if distance < 2400.0 {
+    } else if distance < 900.0 {
         8
-    } else if distance < 4800.0 {
+    } else if distance < 1200.0 {
         16
-    } else {
+    } else if distance < 1400.0 {
         32
+    } else {
+        64
     }
 }
